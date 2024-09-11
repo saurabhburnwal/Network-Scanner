be14b9962afa6db4df9496c2e7c57dd7c91f41b1
Saurabh Burnwal <saurabhburnwal00@gmail.com>
2024-09-11 17:19:28 +0530

    add the get_lan_ips functions that scans for IPs within a given range

--- lan_wan_ip_scan.py
+++ lan_wan_ip_scan.py
@@ -5,3 +5,13 @@ import requests
 import os
 import whois
 
+# Get LAN IPs
+def get_lan_ips():
+    target_ip = input("Enter the target IP range (e.g.,192.168.1.1/24): ")
+    arp = ARP(pdst=target_ip)
+    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+    packet = ether/arp
+
+    result = srp(packet, timeout=3, verbose=0)[0]
+
+    
